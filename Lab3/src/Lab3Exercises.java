a5da7dd9f3d348b0495698989c7f9e668b49f9d1
4015b10 <4015b10@guama.ece.uprm.edu>
2017-11-15 18:29:10 -0400

    Se cambio nombre, tamano y posicion.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,10 +2,10 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		JFrame myFrame = new JFrame("Kevin Lopez");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(900, 400);
+		myFrame.setSize(300, 300);
 		myFrame.setVisible(true);
 	}
 }
